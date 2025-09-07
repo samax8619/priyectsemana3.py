388a7207df55d6956f93daa19ab30d3520eac97d
SAMERU <samuelumbariba@gmail.com>
2025-09-06 23:57:31 -0500

    Delete trabajos3.py/actividad 18.py

--- trabajos3.py/actividad 18.py
+++
@@ -1,9 +0,0 @@
-import random
-humedad = int(input("Ingrese la humedad actual del suelo: "))
-umbral = int(input("Ingrese el umbral deseado de humedad: "))
-
-while humedad < umbral:
-    print(f"Humedad {humedad}% -> Activando riego...")
-    humedad += random.randint(5, 15)
-
-print(f"Humedad alcanzada: {humedad}% -> Riego detenido")
